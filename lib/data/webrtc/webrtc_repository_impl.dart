fd346fcdd9cd0290d894b41bde8cd7f75a8fb64d
Yuliia Zhuravel <zhuravel.dev@gmail.com>
2025-12-17 09:36:59 +0200

    implement WebRTC: create repositories - abstract and impl

---
+++ lib/data/webrtc/webrtc_repository_impl.dart
@@ -0,0 +1,50 @@
+import 'package:ai_voice_assistant/domain/webrtc/webrtc_repository.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+
+class WebRTCRepositoryImpl implements WebRTCRepository {
+  RTCPeerConnection? _peerConnection;
+  MediaStream? _localStream;
+
+  @override
+  Future<void> init() async {
+    _localStream = await navigator.mediaDevices.getUserMedia({
+      'audio': true,
+      'video': false,
+    });
+
+    _peerConnection = await createPeerConnection({
+      'iceServers': [
+        {'urls': 'stun:stun.l.google.com:19302'},
+      ],
+    });
+
+    _peerConnection!.addStream(_localStream!);
+  }
+
+  void setOnIceCandidate(Function(RTCIceCandidate) onCandidate) {
+    _peerConnection?.onIceCandidate = onCandidate;
+  }
+
+  @override
+  Future<RTCSessionDescription> createOffer() async {
+    final offer = await _peerConnection!.createOffer();
+    await _peerConnection!.setLocalDescription(offer);
+    return offer;
+  }
+
+  @override
+  Future<void> setRemoteDescription(RTCSessionDescription description) async {
+    await _peerConnection!.setRemoteDescription(description);
+  }
+
+  @override
+  Future<void> addIceCandidate(RTCIceCandidate candidate) async {
+    await _peerConnection!.addCandidate(candidate);
+  }
+
+  @override
+  void dispose() {
+    _localStream?.dispose();
+    _peerConnection?.close();
+  }
+}
